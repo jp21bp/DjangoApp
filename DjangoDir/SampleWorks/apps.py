caf300ce8d6861a957f406e981ac790d67da751a
jp21bp <89864126+jp21bp@users.noreply.github.com>
2025-11-20 14:36:10 -0500

    Changed foldr again so I can add it to settings.py INSTALLED APPS, which is needed to use templates

--- DjangoDir/SampleWorks/apps.py
+++ DjangoDir/SampleWorks/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class FirstapiappConfig(AppConfig):
+class SampleworksConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'firstAPIApp'
+    name = 'SampleWorks'
